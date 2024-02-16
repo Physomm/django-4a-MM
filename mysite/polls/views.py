c92196381047ee34d9376d71f1a04ae47fe97476
Physomm <michowy@vp.pl>
2024-02-16 09:02:09 +0100

    commit1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,23 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse('''
+        <script>
+            var increas() = function(){
+                alert("hello html world)
+            }
+         </script>
+        <button style="background-color:red" onclick="increas()"> hello world </button>
+        <br></br>
+        <input></input>
+        <br></br>
+        <select></select>
+        
+
+
+        
+        
+        
+
+        
+                        ''')
